cf0429c5ec4c6d0f629a7085e0336429cf057ccb
Kazerlelutin <b@bouteiller.contact>
2024-09-29 13:43:20 +0200

    del timeout

--- functions/api/ws/index.js
+++ functions/api/ws/index.js
@@ -9,18 +9,16 @@ export function onRequest(ctx) {
   const [client, server] = Object.values(webSocketPair)
 
   server.accept()
-  server.addEventListener('message', (event) => {
-    setTimeout(async () => {
-      console.log('Received from client:', await ctx.env.next_track.list())
-      await ctx.env.next_track.put('ws', 'Hello from the server KV')
+  server.addEventListener('message', async (event) => {
+    console.log('Received from client:', await ctx.env.next_track.list())
+    await ctx.env.next_track.put('ws', 'Hello from the server KV')
 
-      server.send(
-        JSON.stringify({
-          channel: '/sync',
-          msg: await ctx.env.next_track.get('ws'),
-        })
-      )
-    }, 2000)
+    server.send(
+      JSON.stringify({
+        channel: '/sync',
+        msg: await ctx.env.next_track.get('ws'),
+      })
+    )
   })
 
   return new Response(null, {
